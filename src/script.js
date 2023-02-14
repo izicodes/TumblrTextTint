3b0b198fcb2d702d7d807b83c5074a8c7eed1d2a
Isabelle Zennah <izennah@griffithsandarmour.com>
2023-02-14 23:59:24 +0000

    💗 Added a listeners to the radio buttons

--- src/script.js
+++ src/script.js
@@ -1,3 +1,12 @@
+// Variables
+var hexChoice = document.querySelector("#hex-code-choice");
+var hexSection = document.querySelector("#hex-code-section");
+
+// If HEX Code choice is selected
+hexChoice.addEventListener("click", function () {
+	hexSection.classList.remove("hide");
+});
+
 // User can not delete the first character of the HEX code input
 function preventDeleteFirstChar(event, input) {
 	const key = event.key;
