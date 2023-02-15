7d89556578d3e6c0ed685980bc0a183bebd5094b
Isabelle Zennah <izennah@griffithsandarmour.com>
2023-02-15 08:49:05 +0000

    💗 JS code behind the rb and worked on first btn

--- src/script.js
+++ src/script.js
@@ -1,10 +1,27 @@
 // Variables
 var hexChoice = document.querySelector("#hex-code-choice");
 var hexSection = document.querySelector("#hex-code-section");
+var colourChoice = document.querySelector("#colour-picker-choice");
+var colourSection = document.querySelector("#colour-picker-section");
+var generateBtn = document.querySelector("#generate-btn");
+
+// Add the disabled styling to the first button + disabling the button entirely
+generateBtn.setAttribute("class", "disabled-btn");
+generateBtn.disabled = true;
 
 // If HEX Code choice is selected
 hexChoice.addEventListener("click", function () {
+	if (!colourSection.classList.contains("hide")) {
+		colourSection.classList.add("hide");
+	}
 	hexSection.classList.remove("hide");
+});
+// If colour picker choice is selected
+colourChoice.addEventListener("click", function () {
+	if (!hexSection.classList.contains("hide")) {
+		hexSection.classList.add("hide");
+	}
+	colourSection.classList.remove("hide");
 });
 
 // User can not delete the first character of the HEX code input
